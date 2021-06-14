3f2903c9bc38c68c819f1affd37bc834e384478d
SamsonMychael <asamsonmichael@gmail.com>
2021-06-13 23:16:08 -0700

    Add project files.

---
+++ MessagingCenterApp/MessagingCenterApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MessagingCenterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<string> MessagingItems { get; set; } = new ObservableCollection<string>();
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Subscribe<Page, DateTime>(this, "tick", (p, DateTime) =>
+         {
+             MessagingItems.Add($"Message Received at {DateTime}");
+         });
+        }
+
+       
+        private async void Button_Clicked_2(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SenderPage());
+        }
+    }
+}
---
+++ MessagingCenterApp/MessagingCenterApp/SenderPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessagingCenterApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SenderPage : ContentPage
+    {
+        public SenderPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send<Page, DateTime>(this, "tick", DateTime.Now);
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
